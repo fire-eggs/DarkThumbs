d164ee4be7ecd48c6971be89aa5ddb64a6536955
unknown <lifeattickville@gmail.com>
2022-05-13 04:31:47 -0400

    Fix issue #42: premature unloading causes Explorer freeze: claim it's not ready to be unloaded.

--- CBXShell/CBXShell.cpp
+++ CBXShell/CBXShell.cpp
@@ -48,7 +48,8 @@ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 // Used to determine whether the DLL can be unloaded by OLE
 STDAPI DllCanUnloadNow(void)
 {
-    return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
+    //return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
+	return S_FALSE;
 }
 
 /////////////////////////////////////////////////////////////////////////////
